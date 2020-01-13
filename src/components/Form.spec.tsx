6847691ed3e57df2ff31abd18cfbc11f351057c5
root <root@LAPTOP-NR7QAL87.localdomain>
2020-01-13 11:28:21 +0100

    feat: add emails using the input field

--- src/components/Form.spec.tsx
+++ src/components/Form.spec.tsx
@@ -1,9 +1,57 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import "@testing-library/jest-dom/extend-expect";
 
 import { Form } from "./Form";
+
+describe(`Email block should be created by`, () => {
+  test(`pressing Enter.`, async () => {
+    const { getByTestId, queryAllByTestId, getByText } = render(<Form />);
+
+    const sampleEmail = "john@miro.com";
+
+    expect(queryAllByTestId("email-block")).toHaveLength(0);
+
+    const formInput = getByTestId("form-input");
+    await userEvent.type(formInput, sampleEmail);
+    fireEvent.keyPress(formInput, { key: "Enter", charCode: 13 });
+
+    expect(queryAllByTestId("email-block")).toHaveLength(1);
+    expect(getByText(sampleEmail)).toBeDefined();
+  });
+
+  test(`pressing Comma.`, async () => {
+    const { getByTestId, queryAllByTestId, getByText } = render(<Form />);
+
+    const sampleEmail = "john@miro.com";
+
+    expect(queryAllByTestId("email-block")).toHaveLength(0);
+
+    const formInput = getByTestId("form-input");
+    await userEvent.type(formInput, sampleEmail);
+    await userEvent.type(formInput, ",");
+
+    expect(queryAllByTestId("email-block")).toHaveLength(1);
+    expect(getByText(sampleEmail)).toBeDefined();
+  });
+
+  test(`losing focus on the input field.`, async () => {
+    const { getByTestId, queryAllByTestId, getByText } = render(<Form />);
+
+    const sampleEmail = "john@miro.com";
+
+    expect(queryAllByTestId("email-block")).toHaveLength(0);
+
+    const formInput = getByTestId("form-input");
+    await userEvent.type(formInput, sampleEmail);
+    fireEvent.blur(formInput);
+
+    expect(queryAllByTestId("email-block")).toHaveLength(1);
+    expect(getByText(sampleEmail)).toBeDefined();
+  });
+});
 
 test(`By clicking "​Add email​" button a random email should be added to the list.`, () => {
   const { getByTestId, queryAllByTestId } = render(<Form />);
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -10,15 +10,58 @@ import "./Form.css";
 export interface FormProps {}
 
 export const Form: React.FC<FormProps> = () => {
-  const [emails, setEmails] = useState<string[]>([
-    // "john@miro.com",
-    // "mary@miro.com"
-  ]);
+  const [emails, setEmails] = useState<string[]>([]);
+  const [emailInput, setEmailInput] = useState<string>("");
+
+  const addEmail = useCallback(
+    (newEmail: string): boolean => {
+      if (newEmail === "") {
+        return false;
+      }
+
+      const hasEmail = emails.find(email => email === newEmail);
+
+      if (hasEmail !== undefined) {
+        return false;
+      }
+
+      setEmails([...emails, newEmail]);
+      return true;
+    },
+    [emails, setEmails]
+  );
+
+  const emailInputChangeHandler: React.ChangeEventHandler<HTMLInputElement> = useCallback(
+    event => {
+      setEmailInput(event.target.value);
+    },
+    [setEmailInput]
+  );
+
+  const emailInputKeyPressHandler: React.KeyboardEventHandler<HTMLInputElement> = useCallback(
+    event => {
+      if (event.key === "Enter" || event.key === ",") {
+        event.preventDefault();
+
+        addEmail(emailInput);
+        setEmailInput("");
+      }
+    },
+    [addEmail, emailInput, setEmailInput]
+  );
+
+  const emailInputBlurHandler: React.FocusEventHandler<HTMLInputElement> = useCallback(
+    event => {
+      addEmail(emailInput);
+      setEmailInput("");
+    },
+    [addEmail, emailInput, setEmailInput]
+  );
 
   const addRandomEmail = useCallback(() => {
     const newEmail = getRandomEmail();
-    setEmails([...emails, newEmail]);
-  }, [emails, setEmails]);
+    addEmail(newEmail);
+  }, [addEmail]);
 
   return (
     <div className="EMAIL-EDITOR-form" data-testid="form">
@@ -38,6 +81,11 @@ export const Form: React.FC<FormProps> = () => {
                 ? "Enter email addresses..."
                 : "add more people..."
             }
+            value={emailInput}
+            onChange={emailInputChangeHandler}
+            onKeyPress={emailInputKeyPressHandler}
+            onBlur={emailInputBlurHandler}
+            data-testid="form-input"
           />
         </div>
       </div>
